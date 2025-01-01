9c6e2e78011b7e3bb8af149ca0e6bfd8f9436006
Sonali Landge <sonalilandge13@gmail.com>
2025-01-01 11:35:29 +0530

    added program to print the number of elements in an array

---
+++ OopsConcept/PrintNumOfArr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class PrintNumOfArr
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine(arr.Length);
+
+              
+            
+        }
+    }
+}
