2be0791db996cc9bbd2de8b0adbd3da41c982a67
Sonali Landge <sonalilandge13@gmail.com>
2025-01-03 14:52:24 +0530

    added program to find smallest number in array

---
+++ OopsConcept/SmallNumInArr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class SmallNumInArr
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int smallest = arr[0];
+            foreach(int i in arr)
+            {
+                if (i<smallest)
+                {
+                    smallest = i ;
+                }
+            };
+            Console.WriteLine("smallest number is" + smallest);
+            
+        }
+    }
+}
