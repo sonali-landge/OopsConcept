599f5e4ded683a3fb544e0ef0b3ab9c7ad6abe55
Sonali Landge <sonalilandge13@gmail.com>
2025-01-03 14:53:43 +0530

    added program to find largest number in array

---
+++ OopsConcept/LargeNumInArr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class LargeNumInArr
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 78, 7, 8, 9, 10 };
+            int largest = arr[0];
+            foreach (int i in arr)
+            {
+                if (i > largest)
+                {
+                    largest = i;
+                   
+                }
+            }
+            Console.WriteLine("largest number is" + largest);
+        }
+    }
+}
