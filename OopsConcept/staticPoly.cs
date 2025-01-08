a491b36465553e0e96de35a594c922850f48d140
Sonali Landge <sonalilandge13@gmail.com>
2025-01-08 22:44:53 +0530

    added program of method overloading (static polymorphism)

---
+++ OopsConcept/staticPoly.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("sum of two int numbers is" + (a + b));
+        }
+        public void Add(double a, double b)
+        {
+            Console.WriteLine("sum of two double numbers is" + (a + b));
+        }
+        public void Add(double a, int b)
+        {
+            Console.WriteLine("sum of int and double numbers is" + (a + b));
+        }
+        public void Add(int a, double b, int c)
+        {
+            Console.WriteLine("sum of two int and  one double number is" + (a + b + c));
+        }
+    }
+        internal class staticPoly
+        {
+            static void Main()
+            {
+                Calculator calculator = new Calculator();
+                calculator.Add(2, 4);
+                calculator.Add(3.5, 5.5);
+                calculator.Add(4.5, 6);
+                calculator.Add(6, 7.5, 2);
+
+
+            }
+        }
+    
+}
