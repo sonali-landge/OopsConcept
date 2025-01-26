fea9ec2191796c6763c973da5ee7d6f58addaf7a
Sonali Landge <sonalilandge13@gmail.com>
2025-01-26 23:30:13 +0530

    MultilevelInheritance Assignment

---
+++ OopsConcept/MultilevelInheritance.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OopsConcept
+{
+    class Person2
+    {   public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void DisplayPerson()
+        {
+            Console.WriteLine("Name and age of person is: " + Name + " " + Age);
+        }
+    }
+    class Teacher : Person2
+    {
+        public string Subject { get; set; }
+
+        public void Teach()
+        {
+            Console.WriteLine("Teacher Name and Subject is: " + Name + " " + Subject);
+        }
+    }
+    class Principal : Teacher
+    {
+        public string CollegeName { get; set; }
+
+        public void ManageCollege()
+        {
+            Console.WriteLine("Principal is managing the : " + CollegeName);
+        }
+
+        public void DisplayPrincipalInfo()
+        {
+            Console.WriteLine($"Principal: {Name}, Age: {Age}, College: {CollegeName}");
+        }
+    }
+    internal class MultilevelInheritance
+    {
+        public static void Main()
+        {
+            Principal principal = new Principal();
+            principal.Name = "ABC";
+            principal.Age = 50;
+            principal.Subject = "Math";
+            principal.CollegeName = "Sveri College";
+            principal.DisplayPerson();
+            principal.Teach();
+            principal.ManageCollege();
+            principal.DisplayPrincipalInfo();
+        }
+    }
+}
