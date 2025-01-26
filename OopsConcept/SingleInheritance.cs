6ed264a782abe071797c831f8184972dbecb1cd4
Sonali Landge <sonalilandge13@gmail.com>
2025-01-26 23:29:39 +0530

    Single Inheritance Assignment

---
+++ OopsConcept/SingleInheritance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    class Person1
+    {
+        public string Name;
+        public int Age;
+  
+    }
+    class Student : Person1
+    {
+        public Student(String name,int age) {
+            Name = name;
+            Age = age;
+        }
+        public void Display()
+        {
+            Console.WriteLine("student name and age is:"+ Name +" "+ Age);
+
+        }
+    }
+    internal class SingleInheritance
+    {
+        public static void Main()
+        {
+            Student student = new Student("sonali", 23);
+            student.Display();
+
+        }
+    }
+}
