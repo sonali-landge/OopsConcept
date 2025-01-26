5be1002deba4e82505e361d2b8c5c2d4b28a85c7
Sonali Landge <sonalilandge13@gmail.com>
2025-01-26 23:27:56 +0530

    String Builder Assignment added

---
+++ OopsConcept/AppendStrToStrBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class AppendStrToStrBuilder
+    {
+        static void Main()
+        {
+            StringBuilder sb=new StringBuilder();
+            sb.Append("Hello ");
+            sb.Append("How ");
+            sb.Append("are ");
+            sb.Append("you");
+            Console.WriteLine(sb.ToString());
+            sb.Replace("Hello", "Hey");
+            Console.WriteLine(sb.ToString());
+            // Hey how are you
+            //now remove "how"
+            sb.Remove(4, 3);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
